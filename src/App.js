31c7d00ce5d4185e6e1db4cfaac85af4555bd15b
Saravana16041999 <saravanakumar16041999@gmail.com>
2023-01-06 08:22:34 +0530

    redid

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import './Index.css'
 import MainTour from './components/MainTour';
 
 function App() {
-  const DataUrl = `https://course-api.com/react-tours-project`
+  // const DataUrl = 
 
   const[tours,settour] = useState([])
 
-  const getTour = async ()=>{
-    const responce = await fetch(DataUrl);
-    const tours = await responce.json();
-    settour(tours);
-  }
-useEffect(()=>{
-  getTour()
-},[])
 
   function Deletetours(id){
     const onedelete = tours.filter((tour)=> tour.id !== id);
     settour(onedelete)
   }
+
+  
+
+
+  useEffect(()=>{
+    const getTour = async ()=>{
+      const responce = await fetch(`https://course-api.com/react-tours-project`);
+      const tours = await responce.json();
+      settour(tours);
+    }
+      getTour();
+  },[])
+
 
 
   return (
